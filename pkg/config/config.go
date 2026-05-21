2e74863f4b14e6666298b8ed059f2baad990c6ce
escaleloisa <eloisa.escal1008@gmail.com>
2026-05-21 20:19:02 +0800

    Feat: Update config for all services

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,16 +1,29 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 type Config struct {
-	Port        string
-	DatabaseURL string
+	Port             string
+	DatabaseURL      string
+	KafkaBrokers     []string
+	ElasticsearchURL string
+	RedisURL         string
+	NoteServiceURL   string
+	ImportPath       string
 }
 
 func Load() *Config {
 	return &Config{
-		Port:        getEnv("PORT", "8080"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/knowledgebase?sslmode=disable"),
+		Port:             getEnv("PORT", "8080"),
+		DatabaseURL:      getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/knowledgebase?sslmode=disable"),
+		KafkaBrokers:     strings.Split(getEnv("KAFKA_BROKERS", "localhost:9092"), ","),
+		ElasticsearchURL: getEnv("ELASTICSEARCH_URL", "http://localhost:9200"),
+		RedisURL:         getEnv("REDIS_URL", "redis://localhost:6379"),
+		NoteServiceURL:   getEnv("NOTE_SERVICE_URL", "http://localhost:8080"),
+		ImportPath:       getEnv("IMPORT_PATH", "./import"),
 	}
 }
 
